eac8c9812c5852e4b62cb07c813401fc57c08a13
The-Repo-Club <wayne6324@gmail.com>
2021-11-24 18:55:52 +0000

    Update
    Signed-off-by: The-Repo-Club <wayne6324@gmail.com>

--- src/main/java/club/therepo/rightclickrepair/commands/RightClickGetGearCommand.java
+++ src/main/java/club/therepo/rightclickrepair/commands/RightClickGetGearCommand.java
@@ -1,11 +1,14 @@
 package club.therepo.rightclickrepair.commands;
 
+import club.therepo.customenchantments.CustomEnchantmentsPlugin;
 import club.therepo.rightclickrepair.RightClickRepair;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -32,46 +35,106 @@ public class RightClickGetGearCommand implements CommandExecutor {
 
         if (sender.isOp() || sender.hasPermission("rightclickrepair.use")) {
             ItemStack AdminAxe = new ItemStack(Material.DIAMOND_AXE);
+            AdminAxe.addUnsafeEnchantment(Enchantment.MENDING, 10);
+            AdminAxe.addUnsafeEnchantment(Enchantment.DIG_SPEED, 10);
+            AdminAxe.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
+            AdminAxe.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10);
+            AdminAxe.addUnsafeEnchantment(Enchantment.KNOCKBACK, 10);
+            AdminAxe.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 10);
+            AdminAxe.addUnsafeEnchantment(Enchantment.LOOT_BONUS_MOBS, 10);
+            if (Bukkit.getServer().getPluginManager().getPlugin("CustomEnchantments") != null) {
+                AdminAxe.addUnsafeEnchantment(CustomEnchantmentsPlugin.TELEPATHY, 10);
+            }
             for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.axe")) {
                 inventory.addItem(nameItem(AdminAxe, messages, messages));
             }
 
             ItemStack AdminSword = new ItemStack(Material.DIAMOND_SWORD);
+            AdminSword.addUnsafeEnchantment(Enchantment.MENDING, 10);
+            AdminSword.addUnsafeEnchantment(Enchantment.DIG_SPEED, 10);
+            AdminSword.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
+            AdminSword.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10);
+            AdminSword.addUnsafeEnchantment(Enchantment.KNOCKBACK, 10);
+            AdminSword.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 10);
+            AdminSword.addUnsafeEnchantment(Enchantment.LOOT_BONUS_MOBS, 10);
+            if (Bukkit.getServer().getPluginManager().getPlugin("CustomEnchantments") != null) {
+                AdminSword.addUnsafeEnchantment(CustomEnchantmentsPlugin.TELEPATHY, 10);
+            }
             for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.sword")) {
                 inventory.addItem(nameItem(AdminSword, messages, messages));
             }
 
             ItemStack AdminPickaxe = new ItemStack(Material.DIAMOND_PICKAXE);
+            AdminPickaxe.addUnsafeEnchantment(Enchantment.MENDING, 10);
+            AdminPickaxe.addUnsafeEnchantment(Enchantment.DIG_SPEED, 10);
+            AdminPickaxe.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
+            AdminPickaxe.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10);
+            AdminPickaxe.addUnsafeEnchantment(Enchantment.KNOCKBACK, 10);
+            AdminPickaxe.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 10);
+            AdminPickaxe.addUnsafeEnchantment(Enchantment.LOOT_BONUS_MOBS, 10);
+            AdminPickaxe.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, 10);
+            if (Bukkit.getServer().getPluginManager().getPlugin("CustomEnchantments") != null) {
+                AdminPickaxe.addUnsafeEnchantment(CustomEnchantmentsPlugin.TELEPATHY, 10);
+            }
             for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.pickaxe")) {
                 inventory.addItem(nameItem(AdminPickaxe, messages, messages));
             }
 
             ItemStack AdminShovel = new ItemStack(Material.DIAMOND_SHOVEL);
+            AdminShovel.addUnsafeEnchantment(Enchantment.MENDING, 10);
+            AdminShovel.addUnsafeEnchantment(Enchantment.DIG_SPEED, 10);
+            AdminShovel.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
+            AdminShovel.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 10);
+            AdminShovel.addUnsafeEnchantment(Enchantment.KNOCKBACK, 10);
+            AdminShovel.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 10);
+            AdminShovel.addUnsafeEnchantment(Enchantment.LOOT_BONUS_MOBS, 10);
+            if (Bukkit.getServer().getPluginManager().getPlugin("CustomEnchantments") != null) {
+                AdminShovel.addUnsafeEnchantment(CustomEnchantmentsPlugin.TELEPATHY, 10);
+            }
             for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.shovel")) {
                 inventory.addItem(nameItem(AdminShovel, messages, messages));
             }
 
             ItemStack AdminChestPlate = new ItemStack(Material.DIAMOND_CHESTPLATE);
+            AdminChestPlate.addUnsafeEnchantment(Enchantment.MENDING, 10);
+            AdminChestPlate.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
             for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.armor")) {
                 inventory.addItem(nameItem(AdminChestPlate, messages, messages));
             }
 
             ItemStack AdminBoots = new ItemStack(Material.DIAMOND_BOOTS);
+            AdminBoots.addUnsafeEnchantment(Enchantment.MENDING, 10);
+            AdminBoots.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
             for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.armor")) {
                 inventory.addItem(nameItem(AdminBoots, messages, messages));
             }
 
             ItemStack AdminHelmet = new ItemStack(Material.DIAMOND_HELMET);
+            AdminHelmet.addUnsafeEnchantment(Enchantment.MENDING, 10);
+            AdminHelmet.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
             for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.armor")) {
                 inventory.addItem(nameItem(AdminHelmet, messages, messages));
             }
 
             ItemStack AdminLeggings = new ItemStack(Material.DIAMOND_LEGGINGS);
+            AdminLeggings.addUnsafeEnchantment(Enchantment.MENDING, 10);
+            AdminLeggings.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
             for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.armor")) {
                 inventory.addItem(nameItem(AdminLeggings, messages, messages));
             }
 
-            ItemStack AdminBook = new ItemStack(Material.BOOK);
+            ItemStack AdminBow = new ItemStack(Material.BOW);
+            AdminBow.addUnsafeEnchantment(Enchantment.MENDING, 10);
+            AdminBow.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
+            AdminBow.addUnsafeEnchantment(Enchantment.ARROW_DAMAGE, 10);
+            AdminBow.addUnsafeEnchantment(Enchantment.ARROW_KNOCKBACK, 10);
+            AdminBow.addUnsafeEnchantment(Enchantment.ARROW_FIRE, 10);
+            AdminBow.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 10);
+            for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.bow")) {
+                inventory.addItem(nameItem(AdminBow, messages, messages));
+            }
+
+            ItemStack AdminBook = new ItemStack(Material.ENCHANTED_BOOK);
             for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("lore.armor")) {
                 inventory.addItem(nameItem(AdminBook, messages, messages));
             }
--- src/main/java/club/therepo/rightclickrepair/listeners/RightClickRepairListener.java
+++ src/main/java/club/therepo/rightclickrepair/listeners/RightClickRepairListener.java
@@ -11,6 +11,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class RightClickRepairListener implements Listener {
 
@@ -30,28 +32,54 @@ public class RightClickRepairListener implements Listener {
 
         if (RightClickRepair.plugin.plugin.getConfig().getBoolean("options.enable")) {
             if (action.equals(Action.RIGHT_CLICK_AIR)) {
-                if(i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.axe")))
-                        || i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.sword")))
-                        || i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.pickaxe")))
-                        || i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.shovel")))) {
-                    for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("accept")) {
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', messages
-                                .replaceAll("%gear%".toLowerCase(), i.getItemMeta().getDisplayName())
-                        ));
+                if (i.getItemMeta().getLore() != null) {
+                    if (i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.axe")))
+                            || i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.sword")))
+                            || i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.pickaxe")))
+                            || i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.shovel")))) {
+                        for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("accept")) {
+                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', messages
+                                    .replaceAll("%gear%".toLowerCase(), i.getItemMeta().getDisplayName())
+                            ));
+                        }
+                        ItemMeta itemMeta = i.getItemMeta();
+                        if (itemMeta instanceof Damageable){
+                            ((Damageable) itemMeta).setDamage((short) 0);
+                        }
+                        i.setItemMeta(itemMeta);
                     }
-                    i.setDurability((short)0);
-                }
-                if(i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.armor")))) {
-                    final ItemStack[] content = player.getInventory().getArmorContents();
-                    for (int b = 0; b < content.length; b++) {
-                        final ItemStack item = content[b];
-                        if (item.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.armor")))){
-                            item.setDurability((short) 0);
-                            content[b] = item;
+                    if (i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.bow")))) {
+                        for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("accept")) {
+                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', messages
+                                    .replaceAll("%gear%".toLowerCase(), i.getItemMeta().getDisplayName())
+                            ));
+                        }
+                        ItemMeta itemMeta = i.getItemMeta();
+                        if (itemMeta instanceof Damageable){
+                            ((Damageable) itemMeta).setDamage((short) 0);
+                        }
+                        i.setItemMeta(itemMeta);
+                    }
+                    if (i.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.armor")))) {
+                        final ItemStack[] content = player.getInventory().getArmorContents();
+                        for (int b = 0; b < content.length; b++) {
+                            final ItemStack item = content[b];
+                            if (item != null) {
+                                if (item.getItemMeta().getLore().toString().equals(ChatColor.translateAlternateColorCodes('&', plugin.plugin.getConfig().getString("lore.armor")))) {
+                                    ItemMeta itemMeta = i.getItemMeta();
+                                    if (itemMeta instanceof Damageable){
+                                        ((Damageable) itemMeta).setDamage((short) 0);
+                                    }
+                                    i.setItemMeta(itemMeta);
+                                }
+                            }
+                        }
+                        for (String messages : RightClickRepair.plugin.plugin.getConfig().getStringList("accept")) {
+                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', messages
+                                    .replaceAll("%gear%".toLowerCase(), i.getItemMeta().getDisplayName())
+                            ));
                         }
                     }
-                    player.getInventory().setArmorContents(content);
-                    player.updateInventory();
                 }
             }
         }
